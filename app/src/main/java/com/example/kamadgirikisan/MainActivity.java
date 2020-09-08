d0d647990af3e59e8e54c57701e2703f7f5d86b8
amit <amit.gla_mca18@gla.ac.in>
2020-09-08 14:37:49 +0530

    Dashboard flow design

---
+++ app/src/main/java/com/example/kamadgirikisan/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.kamadgirikisan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+//import com.example.kamadgirikisan.ui.login.LoginActivity;
+import com.example.kamadgirikisan.ui.login.quizLoginActivity;
+import com.example.kamadgirikisan.ui.login.redemptionLoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView quiz,redemption;
+    Intent quizIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_main);
+        quiz = findViewById(R.id.quiz);
+        redemption = findViewById(R.id.redemption);
+
+        quiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quizIntent = new Intent(MainActivity.this, quizLoginActivity.class);
+                startActivity(quizIntent);
+                Toast.makeText(MainActivity.this, "quiz clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        redemption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                quizIntent = new Intent(MainActivity.this, redempsignup.class);
+                startActivity(quizIntent);
+                Toast.makeText(MainActivity.this, "redemption clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+}
